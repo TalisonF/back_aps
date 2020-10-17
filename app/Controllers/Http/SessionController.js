c1f9a3d457ba9c430564e7ed37945d945f95388a
talisonf <talison737@gmail.com>
2020-10-17 15:17:14 -0300

    get usuario

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,14 @@
 'use strict'
 
+const User = use("App/Models/User")
 class SessionController {
   async create ({ request, auth }) { 
     const { email, password } = request.all()
 
     const token = await auth.attempt(email, password)
-
-    return token
+    const user = await User.findBy('email', email)
+    
+    return {...token, username: user.username}
   }
 }
 
